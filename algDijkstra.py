a29f23046ce5d50cf3b46291aa12c7dc7cef2cd5
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2019-12-12 13:26:13 +0100

    made some other improvements

--- algDijkstra.py
+++ algDijkstra.py
@@ -11,25 +11,40 @@ class nodePriority:
     def __lt__(self, other):
         return self.priority > other.priority
 
-def dijkstra(G, s):
-    neighbours = queue.PriorityQueue(); result = []; edges = np.array(list(G.edges))
-    neighbours.put(nodePriority(0, s))
+def dijkstra(G, s, threshold):
+    neighbours = np.array([[0, s]]); result = []; edges = np.array(list(G.edges))
     for i in G.nodes:
         result.append([-1, 100000])
     result[s-1] = [s, 0]
     result = np.array(result)
-    while not neighbours.empty():
-        node = neighbours.get().node
+    while len(neighbours) != 0:
+        costs = [neighbour[0] for neighbour in neighbours]; cont = 0
+        if min(costs) > threshold:
+            return result
+        for neighbour in neighbours:
+            if neighbour[0] == min(costs):
+                node = neighbour[1]
+                neighbours = np.delete(neighbours, cont, axis = 0)
+            cont += 1
         for edge in edges:
-            if (edge[0] == node or edge[1] == node) and edge[1] != result[node-1][0]:
+            if edge[0] == node and edge[1] != result[node-1][0]:
                 if result[edge[1]-1][0] != -1:
                     if result[edge[1]-1][1] > result[node-1][1] + G.edges[edge[0], edge[1]]["weight"]:
                         result[edge[1] - 1][0] = node
                         result[edge[1] - 1][1] = result[node - 1][1] + G.edges[edge[0], edge[1]]["weight"]
                 else:
-                    neighbours.put(nodePriority(result[node-1][1] + G.edges[edge[0], edge[1]]["weight"], edge[1]))
+                    neighbours = np.append(neighbours, [[result[node-1][1] + G.edges[edge[0], edge[1]]["weight"], edge[1]]], axis = 0)
                     result[edge[1]-1][0] = node
                     result[edge[1]-1][1] = result[node-1][1] + G.edges[edge[0], edge[1]]["weight"]
+            if edge[1] == node and edge[0] != result[edge[1]-1][0]:
+                if result[edge[0]-1][0] != -1:
+                    if result[edge[0]-1][1] > result[edge[1]-1][1] + G.edges[edge[0], edge[1]]["weight"]:
+                        result[edge[0] - 1][0] = node
+                        result[edge[0] - 1][1] = result[edge[1] - 1][1] + G.edges[edge[0], edge[1]]["weight"]
+                else:
+                    neighbours = np.append(neighbours, [[result[edge[1]-1][1] + G.edges[edge[0], edge[1]]["weight"], edge[0]]], axis = 0)
+                    result[edge[0]-1][0] = edge[1]
+                    result[edge[0]-1][1] = result[edge[1]-1][1] + G.edges[edge[0], edge[1]]["weight"]
     return result
 
 G = nx.Graph(); threshold = 10; app = []
@@ -37,9 +52,12 @@ for i in range(1, 10):
     G.add_node(i)
 for i in range(1, 10):
     for j in range(1, 10):
-        if i != j:
+        num = random.randint(0, 10)
+        if num > 6 and i != j:
             G.add_edge(i, j, weight = random.randint(1, 20))
-resultDij = dijkstra(G, 1)
+print(G.edges)
+print([G.edges[edge] for edge in G.edges])
+resultDij = dijkstra(G, 1, 10)
 for i in range(len(resultDij)):
     if resultDij[i][1] < threshold:
         app.append([i+1, resultDij[i][0]])
@@ -47,7 +65,4 @@ result = nx.Graph()
 for i in range(len(app)):
     result.add_node(app[i][0])
     if i > 0: result.add_edge(app[i][0], app[i][1])
-colors = ["red"] + (["cyan"] * (len(result.nodes) - 1)); pos = nx.spring_layout(result)
-nx.draw_networkx_nodes(result, pos, node_color = colors)
-nx.draw_networkx_edges(result, pos)
-nx.draw_networkx_labels(result, pos)
+print(resultDij)
